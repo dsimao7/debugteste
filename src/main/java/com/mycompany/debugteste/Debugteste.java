9c25e462a4792f92ad956dd61502b91f9f2c5c41
diogo <diogo@cookiespc.mshome.net>
2023-03-14 14:42:04 +0000

    primeiro commit

---
+++ src/main/java/com/mycompany/debugteste/Debugteste.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.debugteste;
+
+/**
+ *
+ * @author diogo
+ */
+public class Debugteste {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int factorial = 1;
+ 
+        // n! = 1*2*3...*n
+        for (int i = 1; i <= n; i++) {
+                factorial *= i;
+        }
+        
+        System.out.println("The Factorial of " + n + " is " + factorial);
+    }
+}
